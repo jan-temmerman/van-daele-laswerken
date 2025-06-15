8e90c8508be804437a39a7f95daaef53361ef98f
jantemmerman <temmjan@gmail.com>
2025-06-16 01:19:47 +0200

    changes and addes cases

--- app.vue
+++ app.vue
@@ -13,14 +13,14 @@
         <a href="tel:+32471348815" class="cta">Bel ons</a>
       </div>
 
-      <NuxtImg src="/hero_image.jpg" alt="Man welding stock photo" preload class="hero-image"/>
+      <NuxtImg src="/hero_image.png" alt="Man welding stock photo" preload class="hero-image"/>
     </div>
   </div>
 
   <div class="section section-light section-no-full-height about-us">
     <h2>Over ons</h2>
     <p>Met ondertussen 10 jaar ervaring in de metaalsector bieden wij een snelle en kwalitatieve service.
-      Voor het lassen/herstellen van landbouwmachines kraanbakken aanhangwagens containers… zowel in de werkplaats als ter plaatse.</p>
+      Voor het lassen/herstellen van landbouwmachines, kraanbakken, aanhangwagens, containers, etc... Zowel in de werkplaats als ter plaatse.</p>
 
     <div class="about-us-features">
       <div class="about-us-features-item">
@@ -38,35 +38,73 @@
         <p>Elektrode</p>
       </div>
     </div>
+  </div>
 
-    <div class="divider"/>
-
+  <div class="section section-no-full-height services">
     <h2>Diensten</h2>
 
     <div class="about-us-services">
       <div class="about-us-services-item">
-        <Icon name="material-symbols:tools-wrench-outline-rounded" :size="60"/>
+        <Icon name="material-symbols:tools-wrench-outline-rounded" :size="50"/>
         <p>Herstellingen van metalen constructies</p>
       </div>
 
       <div class="about-us-services-item">
-        <Icon name="material-symbols:delivery-truck-speed-outline" :size="60"/>
+        <Icon name="material-symbols:delivery-truck-speed-outline" :size="50"/>
         <p>Lassen op locatie</p>
       </div>
 
       <div class="about-us-services-item">
-        <Icon name="material-symbols:join-left-rounded" :size="60"/>
+        <Icon name="material-symbols:join-left-rounded" :size="50"/>
         <p>Oplassen van slijtdelen</p>
       </div>
     </div>
   </div>
 
-  <div class="section footer section-no-full-height">
-    <div class="invoice-card">
-      <h2>Offerte</h2>
+  <div class="section section-light section-no-full-height">
+    <h2>Realisaties</h2>
 
-      <p>Neem contact met ons op voor een vrijblijvende offerte of meer informatie over onze diensten.</p>
-      <a href="mailto:vandaele-laswerken@outlook.be" class="cta">Vraag een offerte aan</a>
+    <div class="case-section">
+        <NuxtImg src="/cases/case_1_1.png" alt="Volledig zelfgemaakte Hijsjuk voor het verplaatsen van bomen" loading="lazy" class="case-section-image"/>
+
+      <div class="case-section-text-container">
+        <h3>Hijsjuk voor het verplaatsen van bomen</h3>
+        <p>Tijdens een grote marktrenovatie in Zele zocht de gemeente een goede en veilige manier om de gezonde bomen te verplaatsen. De aannemer contacteerde mij en het resultaat is deze hijsjuk gemaakt voor het verplaatsen van bomen.</p>
+        <NuxtImg src="/cases/case_1_2.png" alt="De zelfgemaakte hijsjuk in actie" loading="lazy" class="case-section-image"/>
+      </div>
+    </div>
+
+    <div class="divider"/>
+
+    <div class="case-section case-section-reversed">
+      <div class="case-section-text-container">
+        <h3>Nieuwe bodem voor zuigwagen</h3>
+        <p>De zuigwagen van deze klant had nood aan een nieuwe bodem, dus hebben wij deze voorzien en opgelast.</p>
+        <NuxtImg src="/cases/case_2_1.png" alt="Zuigwagen met een nieuwe bodem opgelast" loading="lazy" class="case-section-image"/>
+      </div>
+
+      <NuxtImg src="/cases/case_2_2.png" alt="Zuigwagen zonder bodem" loading="lazy" class="case-section-image"/>
+    </div>
+
+    <div class="divider"/>
+
+    <div class="case-section">
+      <NuxtImg src="/cases/case_3_1.png" alt="Connectie die wij aan het duwblad van een machine gelast hebben" loading="lazy" class="case-section-image"/>
+
+      <div class="case-section-text-container">
+        <h3>Afneembare trekhaak op duwblad</h3>
+        <p>Deze klant zocht een oplossing om een trekhaak op dit duwblad te monteren. Wij hebben een connectie stuk op het duwblad gelast waaraan een trekhaak gemonteerd kan worden.</p>
+        <NuxtImg src="/cases/case_3_2.png" alt="Trekhaak die aan onze opgelaste connectie is gemonteerd" loading="lazy" class="case-section-image"/>
+      </div>
+    </div>
+  </div>
+
+  <div class="section footer section-no-full-height">
+    <div class="cta-card">
+      <h2>Laswerk nodig?</h2>
+
+      <p>Neem contact met ons op om je probleem of idee te bespreken of meer informatie over onze diensten te verkrijgen.</p>
+      <a href="mailto:vandaele-laswerken@outlook.be" class="cta">Stuur ons een mail</a>
     </div>
 
     <div class="contact-card">
@@ -74,7 +112,7 @@
 
       <ul>
         <li>
-          Werkplaats: Bevrijdingslaan 114j, Appels 9200
+          Werkplaats: Bevrijdingslaan 114J, Appels 9200
         </li>
         <li>
           <a href="tel:+32471348815">+32 471 34 88 15</a>
@@ -90,10 +128,11 @@
 
 <script setup lang="ts">
   useSeoMeta({
-    title: 'Laswerken Van Daele',
-    ogTitle: 'Laswerken Van Daele',
-    description: 'Laswerken Van Daele biedt betrouwbare en flexibele lasoplossingen voor bedrijven.',
-    ogDescription: 'Laswerken Van Daele biedt betrouwbare en flexibele lasoplossingen voor bedrijven.',
+    title: 'Home',
+    ogTitle: 'Home',
+    ogImage: 'https://van-daele-laswerken.be/ogImage.png',
+    description: 'Mobiele las herstellingen en oplassingen voor bedrijven (staal, inox & aluminium).',
+    ogDescription: 'Mobiele las herstellingen en oplassingen voor bedrijven (staal, inox & aluminium).',
   })
 </script>
 
@@ -123,9 +162,14 @@
     }
   }
 
+  ::selection {
+    background: $accent-color;
+    color: $secondary-color;
+  }
+
   html {
     font-family: 'Figtree', 'Montserrat', Arial, sans-serif;
-    background-color: $primary-color;
+    background-color: $secondary-color;
   }
 
   body {
@@ -141,6 +185,7 @@
     flex-direction: column;
     height: 100vh;
     color: $secondary-color;
+    background-color: $primary-color;
 
     h2 {
       font-size: clamp(2rem, 5vw + 1rem, 4rem);
@@ -210,6 +255,7 @@
       object-position: top;
       padding: 5rem;
       animation: 300ms fadeIn;
+      transform: rotate(180deg);
     }
 
     &-text-container {
@@ -262,11 +308,16 @@
     font-weight: 600;
     margin-top: 2rem;
     animation: 300ms fadeIn;
+    transition: scale 0.1s cubic-bezier(0.4, 0, 0.2, 1);
+
+    &:hover {
+      scale: 1.05;
+    }
   }
 
   .about-us {
     &-features, &-services {
-      margin: 2rem 0 2rem 0;
+      margin-top: 2rem;
       display: flex;
       justify-content: center;
       gap: 10rem;
@@ -298,14 +349,13 @@
       color: $primary-color;
 
       &-item {
-        background-color: $secondary-color;
         max-width: 11rem;
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         border-radius: 2rem;
-        color: $primary-color;
+        color: $secondary-color;
         padding: 0 2rem;
         text-align: center;
         animation: 300ms fadeIn;
@@ -327,11 +377,68 @@
     }
   }
 
+  .services {
+    margin: 0 6rem;
+    padding: 6rem;
+
+    @media screen and (max-width: 1024px) {
+      margin: 5%;
+      padding: 2rem 5%;
+    }
+  }
+
+  .case-section {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 4rem;
+    text-align: left;
+
+    &:first-of-type {
+      margin-top: 4rem;
+    }
+
+    & > &-image {
+      object-fit: cover;
+      width: 100%;
+      aspect-ratio: 1;
+    }
+
+    &-text-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      min-height: 0;
+
+      h3 {
+        font-size: clamp(1.5rem, 3vw + 1rem, 2rem);
+        margin: 0;
+      }
+
+      .case-section-image {
+        object-fit: cover;
+        width: 100%;
+        aspect-ratio: 1;
+      }
+    }
+
+    @media screen and (max-width: 1024px) {
+      grid-template-columns: 1fr;
+
+      & > &-image {
+        width: 100%;
+      }
+
+      &-text-container {
+        width: 100%;
+      }
+    }
+  }
+
   .footer {
     flex-direction: row;
     padding: 0;
 
-    .invoice-card {
+    .cta-card {
       margin: 6rem 3rem 6rem 12rem;
       width: calc(50% - 15rem);
     }
@@ -348,6 +455,10 @@
         color: $primary-color;
         font-family: 'Montserrat', Arial, sans-serif;
         margin-bottom: 1rem;
+
+        &::marker {
+          color: $accent-color;
+        }
       }
 
       a:hover {
@@ -361,9 +472,13 @@
       align-items: center;
       flex-wrap: wrap;
 
-      .invoice-card, .contact-card {
+      .cta-card, .contact-card {
         width: 80%;
         margin: 2rem 0;
+      }
+
+      .contact-card {
+        padding: 1rem;
       }
     }
   }
