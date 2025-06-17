6d2d072adfa2cd68c2e6f24f80419cd360177521
jantemmerman <temmjan@gmail.com>
2025-06-17 14:38:01 +0200

    vercel ssr setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  ssr: true,
+  nitro: {
+    preset: 'vercel'
+  },
   site: {
     url: 'https://van-daele-laswerken.be',
     name: 'Laswerken Van Daele',
