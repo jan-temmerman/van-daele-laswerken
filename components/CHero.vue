c798c6029aeb06b02d759eaf161aec29392c064b
jantemmerman <temmjan@gmail.com>
2025-06-17 14:35:19 +0200

    more optimisations

--- components/CHero.vue
+++ components/CHero.vue
@@ -10,10 +10,31 @@
     </div>
 
     <div class="hero-image-container">
-      <NuxtImg src="/hero_image.png" placeholder="/hero_image_placeholder.png" alt="De werkplaats in Appels" preload quality="40" format="webp" class="hero-image"/>
+      <NuxtImg src="/hero_image.png" :placeholder="blurUrl" alt="De werkplaats in Appels" preload quality="40" format="webp" class="hero-image"/>
       <div class="frame-decoration-bottom"/>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { decode } from 'blurhash'
+
+const blurUrl = ref('')
+const blurhashString = 'LLGuzMEmIqrqE2.8x]%1~px^tRxs'
+
+onMounted(() => {
+  const width = 32
+  const height = 32
+  const pixels = decode(blurhashString, width, height)
+  const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  const ctx = canvas.getContext('2d')
+  if (!ctx) return
+  const imageData = ctx.createImageData(width, height)
+  imageData.data.set(pixels)
+  ctx.putImageData(imageData, 0, 0)
+  blurUrl.value = canvas.toDataURL()
+});
 </script>
